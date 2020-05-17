9b0d7e529515757e14e1ee47c250afd957291b81
BingNa Yang <bingnayang@gmail.com>
2020-05-17 00:19:43 -0400

    Add methods and test code

--- src/com/company/Battleship.java
+++ src/com/company/Battleship.java
@@ -1,12 +1,69 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Battleship {
-    int[] opponentLocation;
+    private int[] opponentLocation = {4,5,6};
+    private int mapSize = 10;
 
     public Battleship() {
     }
 
     public void setOpponentLocation(int[] opponentLocation) {
         this.opponentLocation = opponentLocation;
+    }
+
+    public int[] getOpponentLocation() {
+        return opponentLocation;
+    }
+
+    public int getMapSize() {
+        return mapSize;
+    }
+
+    public void setMapSize(int mapSize) {
+        this.mapSize = mapSize;
+    }
+
+    public void printBattleMap(){
+        System.out.println("1D Battle Map. Opponent hide in these 10 location:");
+        System.out.println("==================================================");
+        for(int i=0; i<mapSize; i++){
+            System.out.print("|_"+i+"_|");
+        }
+        System.out.println();
+        System.out.println("==================================================");
+    }
+    public void printBattleshipLocation(){
+        System.out.println("Opponent BattleShip Location: ");
+        System.out.println("==================================================");
+        for(int i=0; i<mapSize; i++){
+            if(i == opponentLocation[0] || i == opponentLocation[1] || i == opponentLocation[2] ){
+                System.out.print("|_@_|");
+            }else{
+                System.out.print("|_"+i+"_|");
+            }
+        }
+        System.out.println();
+        System.out.println("==================================================");
+    }
+    public void printGuessLocation(int guessLocation){
+        for(int i=0; i<mapSize; i++){
+            if(i == guessLocation){
+                System.out.print("|_X_|");
+            }else{
+                System.out.print("|_"+i+"_|");
+            }
+        }
+        System.out.println();
+    }
+
+    public boolean checkHitGuess(int guessLocation){
+        for (int cell : opponentLocation) {
+            if (cell == guessLocation) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        boolean attackResult = false;
+	    Battleship battleship = new Battleship();
+
+        System.out.println("Welcome to the 1D BattleShip Game");
+        battleship.printBattleMap();
+        System.out.println();
+        System.out.println();
+        System.out.println("Enter your attack location (0-9): ");
+        int userGuess = scanner.nextInt();
+
+        attackResult = battleship.checkHitGuess(userGuess);
+        if(attackResult){
+            System.out.println("It's a Hit");
+            battleship.printGuessLocation(userGuess);
+        }else {
+            System.out.println("It's a No Hit");
+        }
+
+
+
+
+
+
+
+        // Print battleship locations in map
+//        battleship.printBattleshipLocation();
     }
 }
