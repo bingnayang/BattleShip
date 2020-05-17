196e1702941dd6ee898aba12fd19412c5665150b
BingNa Yang <bingnayang@gmail.com>
2020-05-16 20:31:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BattleShip
+ 1D battleship game
---
+++ src/com/company/Battleship.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Battleship {
+    int[] opponentLocation;
+
+    public Battleship() {
+    }
+
+    public void setOpponentLocation(int[] opponentLocation) {
+        this.opponentLocation = opponentLocation;
+    }
+}
