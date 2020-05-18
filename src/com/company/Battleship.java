54328fd8b28f8e26b142d433c106a6b0c5258151
BingNa Yang <bingnayang@gmail.com>
2020-05-18 17:56:39 -0400

    Change method to check result
    Use ArrayList to store opponent location instead of array

--- src/com/company/Battleship.java
+++ src/com/company/Battleship.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Battleship {
-
-    private int[] opponentLocation = {4,5,6};
+    private ArrayList<Integer> opponentLocation = new ArrayList<>(Arrays.asList(4,5,6));
     private int mapSize = 10;
     private int numberOfHit;
 
@@ -15,17 +14,20 @@ public class Battleship {
     public int getNumberOfHit() {
         return numberOfHit;
     }
+    public boolean getOpponentSize(){
+        return opponentLocation.isEmpty();
+    }
 
     public void setNumberOfHit(int numberOfHit) {
         this.numberOfHit = numberOfHit;
     }
 
-    public void setOpponentLocation(int[] opponentLocation) {
-        this.opponentLocation = opponentLocation;
+    public ArrayList<Integer> getOpponentLocation() {
+        return opponentLocation;
     }
 
-    public int[] getOpponentLocation() {
-        return opponentLocation;
+    public void setOpponentLocation(ArrayList<Integer> opponentLocation) {
+        this.opponentLocation = opponentLocation;
     }
 
     public int getMapSize() {
@@ -45,11 +47,12 @@ public class Battleship {
         System.out.println();
         System.out.println("==================================================");
     }
+
     public void printBattleshipLocation(){
         System.out.println("Opponent BattleShip Location: ");
         System.out.println("==================================================");
         for(int i=0; i<mapSize; i++){
-            if(i == opponentLocation[0] || i == opponentLocation[1] || i == opponentLocation[2] ){
+            if(opponentLocation.contains(i)){
                 System.out.print("|_@_|");
             }else{
                 System.out.print("|_"+i+"_|");
@@ -58,6 +61,7 @@ public class Battleship {
         System.out.println();
         System.out.println("==================================================");
     }
+
     public void printGuessLocation(int guessLocation){
         for(int i=0; i<mapSize; i++){
             if(i == guessLocation){
@@ -70,12 +74,24 @@ public class Battleship {
     }
 
     public boolean checkHitGuess(int guessLocation){
-        for (int cell : opponentLocation) {
-            if (cell == guessLocation) {
-                numberOfHit++;
-                return true;
-            }
+//        boolean checkResult = false;
+//        int index = opponentLocation.indexOf(guessLocation);
+//        if(index >= 0){
+//            opponentLocation.remove(index);
+//            if(opponentLocation.isEmpty()){
+//                checkResult = true;
+//            }else {
+//                checkResult = false;
+//            }
+//        }
+//        return checkResult;
+        if(opponentLocation.contains(guessLocation)){
+            opponentLocation.remove(Integer.valueOf(guessLocation));
+            return true;
+        }else {
+            return false;
         }
-        return false;
+
+
     }
 }
